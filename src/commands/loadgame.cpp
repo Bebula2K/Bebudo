1d0f93e56d2e1fed1abfaafb38774d8223442907
Adam <117942532+Bebula2K@users.noreply.github.com>
2024-07-03 17:05:00 +0300

    Code cleanup (oops)

--- src/commands/loadgame.cpp
+++ src/commands/loadgame.cpp
@@ -1,4 +1,4 @@
-#include "savegame.h"
+#include "loadgame.h"
 #include "inventory.h"
 
 #include <iostream>
@@ -10,19 +10,43 @@
 #include <chrono>
 #include <ctime>
 
-void saveGame(){
-    std::ofstream outfile("save.txt");
-    if (outfile.is_open()) {
-        outfile << username << "\n";
-        outfile << money << "\n";
-        outfile << level << "\n";
-        outfile << questsDone << "\n";
-        outfile << dustItem << "\n";
-        outfile << stringItem << "\n";
-        outfile << clayItem << "\n";
-        outfile << clayBowlItem << "\n";
-        outfile << flintItem << "\n";
-        outfile.close();
+void loadGame() {
+    std::ifstream infile("save.txt");
+    if (infile.is_open()) {
+        std::getline(infile, username);
+        
+        std::string moneyStr;
+        std::getline(infile, moneyStr);
+        money = std::stod(moneyStr);
+        
+        std::string levelStr;
+        std::getline(infile, levelStr);
+        level = std::stoi(levelStr); 
+
+        std::string questsDoneStr;
+        std::getline(infile, questsDoneStr);
+        questsDone = std::stoi(questsDoneStr); 
+
+        std::string dustItemStr;
+        std::getline(infile, dustItemStr);
+        dustItem = std::stoi(dustItemStr); 
+
+        std::string stringItemStr;
+        std::getline(infile, stringItemStr);
+        stringItem = std::stoi(stringItemStr); 
+
+        std::string clayItemStr;
+        std::getline(infile, clayItemStr);
+        clayItem = std::stoi(clayItemStr); 
+
+        std::string clayBowlItemStr;
+        std::getline(infile, clayBowlItemStr);
+        clayBowlItem = std::stoi(clayBowlItemStr); 
+
+        std::string flintItemStr;
+        std::getline(infile, flintItemStr);
+        flintItem = std::stoi(flintItemStr); 
+        
+        infile.close();
     }
-    std::cout << "\033[32mGame saved!\033[0m\n";
 }
