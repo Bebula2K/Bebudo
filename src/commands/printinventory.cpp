89769cf5fbc39151771d2bc0b709bf8afc670928
Adam <117942532+Bebula2K@users.noreply.github.com>
2024-07-03 13:30:40 +0300

    Added colors

--- src/commands/printinventory.cpp
+++ src/commands/printinventory.cpp
@@ -5,9 +5,9 @@
 
 void printInventory(){
     std::cout << "\nInventory:\n";
-    std::cout << "{\033[32mDust\033[0m}      > \033[33" << dustItem << "\033[0m\n";
-    std::cout << "{\033[32mString\033[0m}    > \033[33" << stringItem << "\033[0m\n";
-    std::cout << "{\033[32mGravel\033[0m}    > \033[33" << gravelItem << "\033[0m\n";
-    std::cout << "{\033[32mClay\033[0m}      > \033[33" << clayItem << "\033[0m\n";
-    std::cout << "{\033[32mClay bowl\033[0m} > \033[33" << clayBowlItem << "\033[0m\n";
+    std::cout << "{\033[32mDust\033[0m}      > " << dustItem << "\n";
+    std::cout << "{\033[32mString\033[0m}    > " << stringItem << "\n";
+    std::cout << "{\033[32mGravel\033[0m}    > " << gravelItem << "\n";
+    std::cout << "{\033[32mClay\033[0m}      > " << clayItem << "\n";
+    std::cout << "{\033[32mClay bowl\033[0m} > " << clayBowlItem << "\n";
 }
