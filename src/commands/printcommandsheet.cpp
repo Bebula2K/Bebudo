5d08637fc3b9c271af6e4a055c319e8d2e8da61a
Adam <117942532+Bebula2K@users.noreply.github.com>
2024-07-03 12:02:56 +0300

    Changed layout

--- src/commands/printcommandsheet.cpp
+++ src/commands/printcommandsheet.cpp
@@ -4,14 +4,14 @@
 void printCommandSheet(){
     std::cout << "\nCommands:" << std::endl;
     std::cout << "| [C] commands" << std::endl;
-    std::cout << "| [E] exit" << std::endl;
     std::cout << "| [M] stats" << std::endl;
-    std::cout << "| [Q] clear screen" << std::endl;
-    std::cout << "| [R] scavenge" << std::endl;
+    std::cout << "| [F] quests" << std::endl;
     std::cout << "| [I] inventory" << std::endl;
+    std::cout << "| [G] buy items" << std::endl;
     std::cout << "| [D] sell items" << std::endl;
     std::cout << "| [T] craft items" << std::endl;
-    std::cout << "| [F] quests" << std::endl;
-    std::cout << "| [G] shop" << std::endl;
+    std::cout << "| [R] scavenge" << std::endl;
+    std::cout << "| [Q] clear screen" << std::endl;
     std::cout << "| [S] save game" << std::endl;
+    std::cout << "| [E] exit" << std::endl;
 }
