d1951493ac7ca8c5d53ffc43d6d060986e711d28
Adam <117942532+Bebula2K@users.noreply.github.com>
2024-07-03 13:29:06 +0300

    Added colors

--- src/commands/printinventory.cpp
+++ src/commands/printinventory.cpp
@@ -5,9 +5,9 @@
 
 void printInventory(){
     std::cout << "\nInventory:\n";
-    std::cout << "{Dust}      > " << dustItem << "\n";
-    std::cout << "{String}    > " << stringItem << "\n";
-    std::cout << "{Gravel}    > " << gravelItem << "\n";
-    std::cout << "{Clay}      > " << clayItem << "\n";
-    std::cout << "{Clay bowl} > " << clayBowlItem << "\n";
+    std::cout << "{\033[32mDust\033[0m}      > \033[33" << dustItem << "\033[0m\n";
+    std::cout << "{\033[32mString\033[0m}    > \033[33" << stringItem << "\033[0m\n";
+    std::cout << "{\033[32mGravel\033[0m}    > \033[33" << gravelItem << "\033[0m\n";
+    std::cout << "{\033[32mClay\033[0m}      > \033[33" << clayItem << "\033[0m\n";
+    std::cout << "{\033[32mClay bowl\033[0m} > \033[33" << clayBowlItem << "\033[0m\n";
 }
